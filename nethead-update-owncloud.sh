8c823ec17798927270106f1177edcf8ab7dab4ab
Markus Zimmermann <markus.zimmermann@nethead.at>
2014-04-16 18:51:50 +0200

    update scripts for owncloud instances

---
+++ nethead-update-owncloud.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+OWNCLOUD_PATH=$1
+OWNCLOUD_USER=$2
+OWNCLOUD_GROUP=$3
+OWNCLOUD_VERSION=$4
+
+wget http://download.owncloud.org/community/owncloud-$OWNCLOUD_VERSION.tar.bz2
+
+if [ -f owncloud-$OWNCLOUD_VERSION.tar.bz2 ];
+then
+	tar xvjf owncloud-$OWNCLOUD_VERSION.tar.bz2
+	rm -R owncloud/config
+	rm -R owncloud/data
+
+	rsync -a owncloud/ $OWNCLOUD_PATH --progress
+	rm -R owncloud
+	rm owncloud-$OWNCLOUD_VERSION.tar.bz2
+
+	chown -R $OWNCLOUD_USER:$OWNCLOUD_GROUP $OWNCLOUD_PATH
+	chmod -R 750 $OWNCLOUD_PATH
+	chown root:$OWNCLOUD_GROUP $OWNCLOUD_PATH
+else
+   echo "File owncloud-$OWNCLOUD_VERSION.tar.bz2 dos not exists"
+fi

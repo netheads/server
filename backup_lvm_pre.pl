057eef8149004a24e72a88cad6da965a503dd8da
Markus Zimmermann <markus.zimmermann@nethead.at>
2015-08-09 10:55:44 +0200

    Redirect STDERR in the pre mount script too

--- backup_lvm_pre.pl
+++ backup_lvm_pre.pl
@@ -8,18 +8,18 @@ use POSIX;
 
 sub get_logical_volume_information {
 	my ($volume_group) = @_;
-	
-	my @lines = `lvdisplay $volume_group`;
-	
+
+	my @lines = `lvdisplay $volume_group 2>&1`;
+
 	return if not @lines;
-	
+
 	my %vg;
 	my $lv = {};
-	
+
 	for my $line (@lines) {
 		if ($line =~ m/--- Logical volume ---/) {
 			$vg{$lv->{name}} = $lv if %$lv;
-			
+
 			$lv = {};
 		}
 		elsif ($line =~ m/LV Path\s*(\S+)\n/) {
@@ -33,7 +33,7 @@ sub get_logical_volume_information {
 			$lv->{size_unit} = $2;
 		}
 	}
-	
+
 	$vg{$lv->{name}} = $lv if %$lv;
 
 	return %vg;
@@ -43,7 +43,7 @@ my %args = Arguments;
 
 if (not exists $args{backuppath} or not exists $args{vg} or not exists $args{lv}) {
 	print "usage: backup_lvm_pre.pl --backup <backup path> --vg <volume group> --lv (<logical volume>)+\n";
-	
+
 	exit(1);
 }
 
@@ -59,19 +59,23 @@ for my $lv_name (@{$args{lv}}) {
 	my $lv = $vg{$lv_name};
 
 	my $size = $lv->{size};
-	
+
 	# convert to . comma
 	$size =~ s/,/./;
-	
+
 	# round to full number
 	$size =  ceil($size * 0.3);
 
-	`lvcreate -L$size$lv->{size_unit} -s -n backup_$lv_name $lv->{path}`;
-	
+	my $create = `lvcreate -L$size$lv->{size_unit} -s -n backup_$lv_name $lv->{path} 2>&1`;
+	print $create;
+
 	print "/dev/$args{vg}/backup_$lv_name created\n";
-	
-	`mkdir -p $args{backuppath}/$lv_name`;
-	`mount /dev/$args{vg}/backup_$lv_name $args{backuppath}/$lv_name`;
-	
+
+	my $mkdir = `mkdir -p $args{backuppath}/$lv_name 2>&1`;
+	print $mkdir;
+
+	my $mount = `mount /dev/$args{vg}/backup_$lv_name $args{backuppath}/$lv_name 2>&1`;
+	print $mount;
+
 	print "/dev/$args{vg}/backup_$lv_name mounted\n";
 }

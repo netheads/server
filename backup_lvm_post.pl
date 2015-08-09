8aab924dc31169764d006da44f6c02318a2842d3
Markus Zimmermann <markus.zimmermann@nethead.at>
2015-08-09 10:35:38 +0200

    Redirect STDERR and give up if mount is not mounted at all

--- backup_lvm_post.pl
+++ backup_lvm_post.pl
@@ -7,23 +7,23 @@ use Filter::Arguments;
 
 sub get_logical_volume_information {
 	my ($volume_group) = @_;
-	
-	my @lines = `lvdisplay $volume_group`;
-	
+
+	my @lines = `lvdisplay $volume_group 2>&1`;
+
 	return if not @lines;
-	
+
 	my %vg;
 	my $lv = {};
-	
+
 	for my $line (@lines) {
 		if ($line =~ m/--- Logical volume ---/) {
 			$vg{$lv->{name}} = $lv if %$lv;
-			
+
 			$lv = {};
 		}
 		elsif ($line =~ m/LV Path\s*(\S+)\n/) {
 			$lv->{path} = $1;
-			
+
 			$lv->{path} =~ m/\/dev\/$volume_group\/(.+)/;
 			$lv->{name} = $1;
 		}
@@ -32,7 +32,7 @@ sub get_logical_volume_information {
 			$lv->{size_unit} = $2;
 		}
 	}
-	
+
 	$vg{$lv->{name}} = $lv if %$lv;
 
 	return %vg;
@@ -42,7 +42,7 @@ my %args = Arguments;
 
 if (not exists $args{backuppath} or not exists $args{vg} or not exists $args{lv}) {
 	print "usage: backup_lvm_post.pl --backup <backup path> --vg <volume group> --lv (<logical volume>)+\n";
-	
+
 	exit(1);
 }
 
@@ -56,24 +56,30 @@ $args{lv} = [ $args{lv} ] if not ref $args{lv};
 
 for my $lv_name (@{$args{lv}}) {
 	my $lv = $vg{$lv_name};
-	
+
 	while (1) {
 		if (-d "$args{backuppath}/$lv_name") {
-			`umount $args{backuppath}/$lv_name`;
-			
+			my $umount = `umount $args{backuppath}/$lv_name 2>&1`;
+			print $umount;
+
+			if ($umount =~ m/not mounted/) {
+				last;
+			}
+
 			print "$args{backuppath}/$lv_name unmounted\n";
 		}
-		
-		`lvremove -f /dev/$args{vg}/backup_$lv_name`;
-		
+
+		my $remove = `lvremove -f /dev/$args{vg}/backup_$lv_name 2>&1`;
+		print $remove;
+
 		if (not -l "/dev/$args{vg}/backup_$lv_name") {
 			print "/dev/$args{vg}/backup_$lv_name removed\n";
-			
+
 			last;
 		}
-		
+
 		print "retrying in 2 seconds\n";
-		
+
 		sleep 2;
 	}
 }
